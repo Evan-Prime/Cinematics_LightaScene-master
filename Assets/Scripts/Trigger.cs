21f8ab167fb93f9ed9fd8da3bbea6a06ca707e5c
Evan MacDonald <113189803+Evan-Prime@users.noreply.github.com>
2022-11-18 05:15:09 -0400

    Finished Trigger set-up, and Set-up player path
    Need to add Animation, and Sound

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,31 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Trigger : MonoBehaviour
 {
-    
+    public PlayableDirector timeline;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool OneTimeTrigger = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        
         if (other.gameObject.tag == "Player")
         {
-            //timeline.Play();
+            timeline.Play();
 
-            // only use if you want to have an event that only triggers once
             if (OneTimeTrigger == true)
             {
                 Destroy(this.gameObject);
